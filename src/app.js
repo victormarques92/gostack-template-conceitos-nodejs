32bb0f160c509c67acad3cb4dbcaa1d667e33d91
Victor Marques <victor.marques@docway.co>
2020-06-04 15:33:01 -0300

    desafio concluído

---
+++ src/app.js
@@ -0,0 +1,91 @@
+const express = require("express");
+const cors = require("cors");
+
+const { uuid } = require("uuidv4");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const repositories = [];
+
+app.get("/repositories", (request, response) => {
+  const { title } = request.query
+
+  const results = title
+    ? repositories.filter(repo => repo.title.includes(title))
+    : repositories
+
+  return response.json(results)
+});
+
+app.post("/repositories", (request, response) => {
+  const { title, url, techs } = request.body
+
+  const repository = { 
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+
+  repositories.push(repository)
+
+  return response.json(repository)
+});
+
+app.put("/repositories/:id", (request, response) => {
+  const { id } = request.params
+  const { title, url, techs } = request.body
+
+  const repositoryIndex = repositories.findIndex(repo => repo.id === id)
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found!' })
+  }
+
+  const repository = { 
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repositoryIndex].likes
+  }
+
+  repositories[repositoryIndex] = repository
+
+  return response.json(repository)
+});
+
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params
+
+  const repositoryIndex = repositories.findIndex(repo => repo.id === id)
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found!' })
+  }
+
+  repositories.splice(repositoryIndex, 1)
+
+  return response.status(204).send()
+});
+
+app.post("/repositories/:id/like", (request, response) => {
+  const { id } = request.params
+  
+  const repositoryIndex = repositories.findIndex(repo => repo.id === id)
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found!' })
+  }
+
+  // repositories[repositoryIndex].likes++
+  repositories[repositoryIndex].likes += 1
+
+  return response.json(repositories[repositoryIndex])
+});
+
+module.exports = app;
